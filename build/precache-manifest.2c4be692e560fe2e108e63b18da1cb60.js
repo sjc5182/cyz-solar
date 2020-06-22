6e59fba5bc7252f3c66340467e849ac151cf9f50
sjc5182 <shiguangchen1987@gmail.com>
2020-06-21 21:21:25 -0700

    push the build file

---
+++ build/precache-manifest.2c4be692e560fe2e108e63b18da1cb60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3ccd077235e62156d64bcb0534d73a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74251699dc59404daa12",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "794ce888f05b47332371",
+    "url": "/static/js/2.6423f30c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6423f30c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74251699dc59404daa12",
+    "url": "/static/js/main.a5a4639d.chunk.js"
+  },
+  {
+    "revision": "f366a164126a495b7936",
+    "url": "/static/js/runtime-main.03d32067.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
