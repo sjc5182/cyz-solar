0739612894f06a20944632a4d6ff494b11daef4b
sjc5182 <shiguangchen1987@gmail.com>
2020-06-27 13:19:18 -0700

    add svg files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { Container } from "./Styled/Container";
+import { Section, Container, Layout } from "./Styled/Container";
+import { createGlobalStyle } from "styled-components";
+import Logo from "./SVG/Icon/Logo.svg";
+import Hamburger from "./SVG/Icon/Hambuger.svg";
 import "./App.css";
+
+// const GlobalStyle = createGlobalStyle`
+//   body {
+//     margin: ${(props) => (props.zeroMar ? "0px" : "0px")}
+//   }
+// `;
 
 function App() {
   return (
-    <Container>
-      <div>hello</div>
-    </Container>
+    <Section>
+      <Container>
+        <img src={Logo} alt="Logo" />
+        <img src={Hamburger} alt="Hamburger" />
+      </Container>
+    </Section>
   );
 }
 
--- src/Styled/Container.js
+++ src/Styled/Container.js
@@ -1,6 +1,29 @@
 import styled from "styled-components";
 
+export const Section = styled.div`
+  max-width: 100vw;
+  height: 100vh;
+`;
+
 export const Container = styled.div`
-  max-width: 90vw;
-  margin: 0 auto;
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  img {
+    margin: 10px 15px;
+  }
+
+  @media (min-width: 768px) {
+    img {
+      margin: 10px 30px;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    img {
+      margin: 10px 45px;
+    }
+  }
 `;
