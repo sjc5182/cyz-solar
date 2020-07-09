762b6368c1dc25ae7666de321dc51b92903ce3a2
sjc5182 <shiguangchen1987@gmail.com>
2020-07-08 21:15:12 -0700

    font update

--- src/Styled/Container.js
+++ src/Styled/Container.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Section = styled.div`
   max-width: 100vw;
   height: 100vh;
+  font-family: "Poppins", sans-serif;
 `;
 
 export const Container = styled.div`
